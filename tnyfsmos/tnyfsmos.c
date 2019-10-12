d076602f6079254f47db64fb190c1080f3ea5666
Qi Wenmin <qiwenmin@gmail.com>
2019-10-12 09:59:11 +0800

    Bugfix: delay 0ms as 65535ms.

--- tnyfsmos/tnyfsmos.c
+++ tnyfsmos/tnyfsmos.c
@@ -95,9 +95,9 @@ void tfo_init_os() {
 extern volatile unsigned int _millis;
 
 void timer2_isr() __interrupt 12 {
-    _millis ++;
-
     process_delay_waiting();
+
+    _millis ++;
 }
 
 void init_timer() {
@@ -125,10 +125,11 @@ void init_timer() {
 extern volatile unsigned int _millis;
 
 void timer4_millis_isr() __interrupt(TIM4_ISR) {
-    _millis ++;
     clear_bit(TIM4_SR, TIM4_SR_UIF);
 
     process_delay_waiting();
+
+    _millis ++;
 }
 
 void init_timer() {
